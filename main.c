9db0f7f43f707c949108cf613004bb6c06e6aa1a
Rhodrigo <rhodrigo.leandro@gmail.com>
2024-12-13 13:30:19 -0300

    Funções Básicas Concluídas

--- main.c
+++ main.c
@@ -105,8 +105,16 @@ void criarFuncionario(Funcionario *funcionarios, int *quantidade, int maxFunc)
     newFunc->id = idFunc;
     printf("ID: %d\n", idFunc);
 
-    printf("CPF (Apenas numeros.):");
-    scanf("%11s", newFunc->cpf);
+    do
+    {
+        printf("CPF (Apenas numeros.):");
+        scanf(" %[^\n]", newFunc->cpf);
+        if (strlen(newFunc->cpf) != 11)
+        {
+            printf("Erro: CPF invalido! \nTente novamente.\n");
+        }
+
+    } while (strlen(newFunc->cpf) != 11);
 
     printf("Nome: ");
     scanf(" %[^\n]", newFunc->nome);
@@ -132,6 +140,7 @@ void criarFuncionario(Funcionario *funcionarios, int *quantidade, int maxFunc)
     salvarDados(funcionarios, *quantidade, "funcionarios.txt");
 }
 
+// Serve para remover funcionário do banco de dados
 void removerFuncionario(Funcionario *funcionarios, int *quantidade)
 {
     int id, idDelete = 0;
@@ -151,7 +160,7 @@ void removerFuncionario(Funcionario *funcionarios, int *quantidade)
             printf("CPF: %s\n", funcionarios[i].cpf);
             printf("Nome: %s\n", funcionarios[i].nome);
             printf("Data de Nascimento: %d/%d/%d\n", funcionarios[i].nascimento[0], funcionarios[i].nascimento[1], funcionarios[i].nascimento[2]);
-            printf("Funaco: %s\n", funcionarios[i].funcao);
+            printf("Funcao: %s\n", funcionarios[i].funcao);
             printf("Salario: %.2f\n", funcionarios[i].remuneracao);
             printf("================================================\n");
             printf("Voce deseja remover o usuario acima? (S/N)");
@@ -182,10 +191,172 @@ void removerFuncionario(Funcionario *funcionarios, int *quantidade)
     salvarDados(funcionarios, *quantidade, "funcionarios.txt");
 }
 
+// Serve para pesquisar um funcionário através do seu nome
+void pesquisarFuncionario(Funcionario *funcionarios, int *quantidade)
+{
+    int encontrado = 0;
+    char pesquisado[50];
+    printf("Pesquise o funcionario pelo nome\n");
+    printf("Pesquisar: ");
+    scanf(" %[^\n]", pesquisado);
+
+    for (int i = 0; i < quantidade; i++)
+    {
+        if (strstr(funcionarios[i].nome, pesquisado) != NULL)
+        {
+            printf("ID: %d\n", funcionarios[i].id);
+            printf("CPF: %s\n", funcionarios[i].cpf);
+            printf("Nome: %s\n", funcionarios[i].nome);
+            printf("Data de Nascimento: %d/%d/%d\n", funcionarios[i].nascimento[0], funcionarios[i].nascimento[1], funcionarios[i].nascimento[2]);
+            printf("Funcao: %s\n", funcionarios[i].funcao);
+            printf("Salario: %.2f\n", funcionarios[i].remuneracao);
+            printf("================================================\n");
+            encontrado = 1;
+        }
+    }
+    if (!encontrado)
+    {
+        printf("Nao existe nenhuma funcionario com o nome: %s\n", pesquisado);
+    }
+}
+
+// Função para listar informações de todos os funcionários cadastrados
+void listarFuncionarios(Funcionario *funcionarios, int *quantidade)
+{
+
+    if (*quantidade == 0)
+    {
+        printf("Nenhum funcionario cadastrado.\n");
+    }
+    else
+    {
+        printf("Lista de Funcionarios\n");
+        printf("================================================\n");
+        for (int i = 0; i < *quantidade; i++)
+        {
+            printf("ID: %d\n", funcionarios[i].id);
+            printf("CPF: %s\n", funcionarios[i].cpf);
+            printf("Nome: %s\n", funcionarios[i].nome);
+            printf("Data de Nascimento: %d/%d/%d\n", funcionarios[i].nascimento[0], funcionarios[i].nascimento[1], funcionarios[i].nascimento[2]);
+            printf("Funaco: %s\n", funcionarios[i].funcao);
+            printf("Salario: %.2f\n", funcionarios[i].remuneracao);
+            printf("================================================\n");
+        }
+    }
+}
+
+//Função que altera os dados dos funcionarios
+void alterarFuncionario(Funcionario *funcionarios, int *quantidade)
+{
+
+    int id, encontrado = 0, campoAlterado;
+    char confirme;
+
+    printf("Alterar funcionario\n");
+    printf("Digite o ID do funcionario: ");
+    scanf("%d", &id);
+
+    for (int i = 0; i < quantidade; i++)
+    {
+        if (funcionarios[i].id == id)
+        {
+            encontrado = 1;
+            system("clear");
+            printf("Funcionario: ");
+            printf("ID: %d\n", funcionarios[i].id);
+            printf("CPF: %s\n", funcionarios[i].cpf);
+            printf("Nome: %s\n", funcionarios[i].nome);
+            printf("Data de Nascimento: %d/%d/%d\n", funcionarios[i].nascimento[0], funcionarios[i].nascimento[1], funcionarios[i].nascimento[2]);
+            printf("Funcao: %s\n", funcionarios[i].funcao);
+            printf("Salario: %.2f\n", funcionarios[i].remuneracao);
+            printf("================================================\n");
+            printf("Voce deseja modificar o usuario acima? (S/N)");
+            scanf(" %c", &confirme);
+
+            switch (confirme)
+            {
+            case 's':
+            case 'S':
+                printf("Qual campo será modificado?\n");
+                printf("1 - CPF\n");
+                printf("2 - Nome\n");
+                printf("3 - Data de Nascimento\n");
+                printf("4 - Funcao\n");
+                printf("5 - Salario\n");
+                printf("0 - Cancelar\n");
+                printf("Escolha: ");
+                scanf(" %d", &campoAlterado);
+
+                switch (campoAlterado)
+                {
+                case 1:
+                    do
+                    {
+                        printf("Novo CPF (Apenas numeros.):");
+                        scanf(" %[^\n]", funcionarios[i].cpf);
+                        if (strlen(funcionarios[i].cpf) != 11)
+                        {
+                            printf("Erro: CPF invalido! \nTente novamente.\n");
+                        }
+
+                    } while (strlen(funcionarios[i].cpf) != 11);
+                    break;
+
+                case 2:
+                    printf("Novo nome: ");
+                    scanf(" %[^\n]", funcionarios[i].nome);
+                    break;
+
+                case 3:
+                    printf("Nova Data de Nascimento: \n");
+                    printf("Dia: ");
+                    scanf(" %d", &funcionarios[i].nascimento[0]);
+                    printf("Mes: ");
+                    scanf(" %d", &funcionarios[i].nascimento[1]);
+                    printf("Ano: ");
+                    scanf(" %d", &funcionarios[i].nascimento[2]);
+                    break;
+
+                case 4:
+                    printf("Nova Funcao: ");
+                    scanf(" %[^\n]", funcionarios[i].funcao);
+                    break;
+
+                case 5:
+                    printf("Novo Salario: ");
+                    scanf(" %f", &funcionarios[i].remuneracao);
+                    break;
+
+                default:
+                    printf("Opcao Invalida.\n Tente Novamente.");
+                    return;
+                }
+
+                printf("Funcionario de ID: %d modificado com suecsso!\n", id);
+                salvarDados(funcionarios, quantidade, "funcionarios.txt");
+                break;
+            case 'n':
+            case 'N':
+                printf("Voltando a tela Inicial...\n");
+                return;
+            default:
+                printf("Opcao Invalida.\n");
+                break;
+            }
+        }
+    }
+    if (!encontrado)
+    {
+        printf("Nao existe funcionario com o ID: %d\n", id);
+    }
+    
+}
+
+// Função principal onde todas as outras funções serão executadas
 int main()
 {
     Funcionario funcionarios[50];
-    int quantidade = 0, opt;
+    int quantidade = 0, opc;
     char enter;
 
     inicializarDados(funcionarios, 50);
@@ -197,11 +368,14 @@ int main()
         printf("\n====================================\n");
         printf("1 - Adicionar Funcionario\n");
         printf("2 - Remover Funcionario\n");
+        printf("3 - Pesquisar Funcionario\n");
+        printf("4 - Listar Funcionarios\n");
+        printf("5 - Modificar Funcionario\n");
         printf("0 - Sair\n");
         printf("Escolha: ");
-        scanf("%d", &opt);
+        scanf("%d", &opc);
 
-        switch (opt)
+        switch (opc)
         {
 
         case 1:
@@ -219,6 +393,27 @@ int main()
             getchar();
             scanf("%c", &enter);
             break;
+        case 3:
+            system("clear");
+            pesquisarFuncionario(funcionarios, quantidade);
+            printf("Pressione ENTER para continuar...");
+            getchar();
+            scanf("%c", &enter);
+            break;
+        case 4:
+            system("clear");
+            listarFuncionarios(funcionarios, &quantidade);
+            printf("Pressione ENTER para continuar...");
+            getchar();
+            scanf("%c", &enter);
+            break;
+        case 5:
+            system("clear");
+            alterarFuncionario(funcionarios, quantidade);
+            printf("Pressione ENTER para continuar...");
+            getchar();
+            scanf("%c", &enter);
+            break;
         case 0:
             system("clear");
             printf("Saindo...");
@@ -231,7 +426,7 @@ int main()
             scanf("%c", &enter);
             break;
         }
-    } while (opt != 0);
+    } while (opc != 0);
 
     return 0;
 }
